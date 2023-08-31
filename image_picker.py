cfb13e54a3af34270ec2abe143eabff6d92a7ed7
JPPhoto <jpollack@jpollackphoto.com>
2023-08-30 22:51:12 -0500

    Updated to work with InvokeAI 3.1

--- image_picker.py
+++ image_picker.py
@@ -1,44 +1,39 @@
 # Copyright (c) 2023 Jonathan S. Pollack (https://github.com/JPPhoto)
 
-from typing import Literal
+from pydantic import BaseModel
 
-from pydantic import BaseModel, Field
-
-from ..models.image import (
-    ImageField
+from invokeai.app.invocations.primitives import (
+    ImageField,
 )
 
 from invokeai.app.invocations.baseinvocation import (
     BaseInvocation,
     BaseInvocationOutput,
+    InputField,
     InvocationContext,
-    InvocationConfig,
+    invocation,
+    invocation_output,
+    OutputField,
+    UIType,
 )
 
 import random
 
 
+@invocation_output("image_picker_output")
 class ImagePickerOutput(BaseInvocationOutput):
     """Used to randomly pick from a collection of Images."""
 
-    # fmt: off
-    type: Literal["image_picker_output"] = "image_picker_output"
-    image: ImageField = Field(description="The image being chosen")
-    # fmt: on
+    image: ImageField = OutputField(description="The image being chosen")
 
-    class Config:
-        schema_extra = { "required": [ "type", "image" ] }
 
+@invocation("image_picker", title="ImagePicker", tags=["random", "image_picker"])
 class ImagePickerInvocation(BaseInvocation):
     """Picks one image at random from a list of images"""
 
-    # fmt: off
-    type: Literal["image_picker"] = "image_picker"
-    collection: list[ImageField] = Field(description="The list of images to select from", default_factory=list)
-    # fmt: on
-
-    class Config(InvocationConfig):
-        schema_extra = { "ui": { "title": "ImagePicker", "tags": [ "random", "image_picker" ] } }
+    collection: list[ImageField] = InputField(
+        description="The list of images to select from", ui_type=UIType.ImageCollection, default_factory=list
+    )
 
     def invoke(self, context: InvocationContext) -> ImagePickerOutput:
         return ImagePickerOutput(image=random.choice(self.collection))
